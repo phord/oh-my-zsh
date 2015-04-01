de3a599726fbcc292995d96856621d5e5737e331
Phil Hord <hordp@cisco.com>
2015-04-01 16:51:47 -0400

    git: describe branch more aggressively
    In case we do not have a symbolic-ref for the HEAD commit, try getting
    any remotes or tags for it instead using --all.  Ultimately fall back
    to getting any valid ref with --always.
    
    Strip any resulting refs/heads or refs/heads/remotes prefixes from
    the final result.

--- lib/git.zsh
+++ lib/git.zsh
@@ -3,7 +3,10 @@ function git_prompt_info() {
   local ref
   if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+    ref=$(command git describe --all HEAD 2> /dev/null) || \
     ref=$(command git describe --always HEAD 2> /dev/null) || return 0
+    ref=${ref#refs/heads/}
+    ref=${ref#remotes/}
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
   fi
 }
