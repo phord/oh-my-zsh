4748933e2797a03e98d84bb5429a80e0d097bd41
Phil Hord <phord@purestorage.com>
2021-11-24 19:23:10 -0800

    Fixup for borked git.zsh during rebase

--- lib/git.zsh
+++ lib/git.zsh
@@ -19,7 +19,7 @@ function git_prompt_info() {
 
   local ref
   ref=$(__git_prompt_git symbolic-ref --short HEAD 2> /dev/null) \
-  || ref=$(__git_prompt_git describe --all HEAD 2> /dev/null) || \
+  || ref=$(__git_prompt_git describe --all HEAD 2> /dev/null) \
   || ref=$(__git_prompt_git rev-parse --always HEAD 2> /dev/null) \
   || return 0
   
